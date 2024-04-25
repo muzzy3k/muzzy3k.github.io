a52476e732047110d089f5a3c9ebc3acfa3ea3a5
muzzy3k <144183246+muzzy3k@users.noreply.github.com>
2024-04-25 19:58:28 -0400

    updates to my projects

--- js/projects.js
+++ js/projects.js
@@ -22,30 +22,8 @@
 			// do not open project since github button was clicked
 		} else {
 			switch (this.id) {
-				case "yolo-cpp-api": {
-					return window.open("https://github.com/ethanlaj/yolo-cpp-api");
-				}
-				case "couple-trivia": {
-					return window.open("https://youtu.be/99za1ogBe78");
-				}
-				case "labshare": {
-					return window.open("https://labshare.xyz");
-				}
-				case "checkout-system": {
-					href = "/Projects/checkout-system/checkout-system.html";
-					break;
-				}
-				case "amazon-affiliate-bot": {
-					href = "/Projects/amazon-affiliate-bot/amazon-affiliate-bot.html";
-					break;
-				}
-				case "resell-bot": {
-					href = "/Projects/resell-bot/resell-bot.html";
-					break;
-				}
-				case "techops": {
-					href = "/Projects/techops/techops.html";
-					break;
+				case "gone-phishing": {
+					return window.open("https://github.com/muzzy3k/Gone-Phishing");
 				}
 			}
 
